feccafffd16c8cf08c63334596f82f032bca8eb0
azukiazusa1 <cycleippei@gmail.com>
2021-01-31 17:48:46 +0900

    forgot password

--- src/pages/forgot-password.tsx
+++ src/pages/forgot-password.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import { Box, Button } from "@chakra-ui/react";
+import { Formik, Form } from "formik";
+import { withUrqlClient } from "next-urql";
+import React, { useState } from "react";
+import { InputField } from "../components/InputField";
+import { Wrapper } from "../components/Wrapper";
+import { useForgotPasswordMutation } from "../generated/graphql";
+import { createUrqlClient } from "../utils/crateUrqlClient";
 
 const ForgotPassword: React.FC<{}> = ({}) => {
-  return <div>forgot password</div>;
+  const [complete, setComplete] = useState(false);
+  const [, forgotPassword] = useForgotPasswordMutation();
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ email: "" }}
+        onSubmit={async (values) => {
+          await forgotPassword(values);
+          setComplete(true);
+        }}
+      >
+        {({ isSubmitting }) =>
+          complete ? (
+            <Box>
+              if an account with that email exists, we sent you can emai
+            </Box>
+          ) : (
+            <Form>
+              <InputField
+                name="email"
+                placeholder="email"
+                label="Email"
+                type="email"
+              />
+              <Button
+                mt={4}
+                type="submit"
+                colorScheme="teal"
+                isLoading={isSubmitting}
+              >
+                Forgot Password
+              </Button>
+            </Form>
+          )
+        }
+      </Formik>
+    </Wrapper>
+  );
 };
 
-export default ForgotPassword
+export default withUrqlClient(createUrqlClient)(ForgotPassword);
