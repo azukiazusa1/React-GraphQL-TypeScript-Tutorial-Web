d61856578d389ecda1b30d780c7b1759d81d3b3f
azukiazusa1 <cycleippei@gmail.com>
2021-01-31 14:22:31 +0900

    withUrqlClient

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -6,6 +6,8 @@ import { Wrapper } from "../components/Wrapper";
 import { useLoginMutation } from "../generated/graphql";
 import { toErrorMap } from "../utils/toErrorMap";
 import { useRouter } from 'next/router'
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../utils/crateUrqlClient";
 
 interface registerProps {}
 
@@ -57,4 +59,4 @@ const Login: React.FC<registerProps> = ({}) => {
   );
 };
 
-export default Login;
+export default withUrqlClient(createUrqlClient)(Login);
