6d60b9b2a498f75101b7fcad578af17080b9b926
azukiazusa1 <cycleippei@gmail.com>
2021-02-01 23:01:16 +0900

    Global Error Handling

--- src/pages/create-post.tsx
+++ src/pages/create-post.tsx
@@ -1,24 +1,27 @@
 import { Box, Button } from "@chakra-ui/react";
-import { Formik, Form } from "formik";
+import { Form, Formik } from "formik";
+import { withUrqlClient } from "next-urql";
+import { useRouter } from "next/router";
 import React from "react";
 import { InputField } from "../components/InputField";
-import { useCreatePostMutation } from "../generated/graphql";
-import { toErrorMap } from "../utils/toErrorMap";
-import { useRouter } from "next/router";
-import { withUrqlClient } from "next-urql";
-import { createUrqlClient } from "../utils/crateUrqlClient";
 import { Layout } from "../components/Layout";
+import { useCreatePostMutation } from "../generated/graphql";
+import { createUrqlClient } from "../utils/crateUrqlClient";
+import { useIsAuth } from "../utils/useIsAuth";
 
 const CreatePost: React.FC<{}> = ({}) => {
   const router = useRouter();
+  useIsAuth();
   const [, createPost] = useCreatePostMutation();
   return (
     <Layout variant="small">
       <Formik
         initialValues={{ title: "", text: "" }}
         onSubmit={async (values, { setErrors }) => {
-          await createPost({ input: values });
-          router.push("/");
+          const { error } = await createPost({ input: values });
+          if (!error) {
+            router.push("/");
+          }
         }}
       >
         {({ isSubmitting }) => (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,23 @@
 import { withUrqlClient } from "next-urql";
-import { NavBar } from "../components/NavBar";
+import React from "react";
 import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/crateUrqlClient";
+import NextLink from 'next/link'
+import { Link } from "@chakra-ui/react";
+import { Layout } from "../components/Layout";
 
 const Index = () => {
   const [{ data }] = usePostsQuery();
   return (
-    <>
-      <NavBar />
+    <Layout>
+      <NextLink href="create-post">
+      <Link>
+        create post
+      </Link>
+      </NextLink>
       <div>Hello</div>
       {!data ? null : data.posts.map((p) => <div key={p.id}>{p.title}</div>)}
-    </>
+    </Layout>
   );
 };
 export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
