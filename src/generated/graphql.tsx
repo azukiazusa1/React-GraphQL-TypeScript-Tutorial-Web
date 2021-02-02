37766712ce6488327389c3188dd6eb2bb5d052ff
azukiazusa1 <cycleippei@gmail.com>
2021-02-02 21:27:21 +0900

    card ui

--- src/generated/graphql.tsx
+++ src/generated/graphql.tsx
@@ -23,6 +23,12 @@ export type Query = {
 };
 
 
+export type QueryPostsArgs = {
+  cursor?: Maybe<Scalars['String']>;
+  limit: Scalars['Int'];
+};
+
+
 export type QueryPostArgs = {
   id: Scalars['Float'];
 };
@@ -225,14 +231,17 @@ export type MeQuery = (
   )> }
 );
 
-export type PostsQueryVariables = Exact<{ [key: string]: never; }>;
+export type PostsQueryVariables = Exact<{
+  limit: Scalars['Int'];
+  cursor?: Maybe<Scalars['String']>;
+}>;
 
 
 export type PostsQuery = (
   { __typename?: 'Query' }
   & { posts: Array<(
     { __typename?: 'Post' }
-    & Pick<Post, 'id' | 'createdAt' | 'updatedAt' | 'title'>
+    & Pick<Post, 'id' | 'createdAt' | 'updatedAt' | 'title' | 'text'>
   )> }
 );
 
@@ -340,12 +349,13 @@ export function useMeQuery(options: Omit<Urql.UseQueryArgs<MeQueryVariables>, 'q
   return Urql.useQuery<MeQuery>({ query: MeDocument, ...options });
 };
 export const PostsDocument = gql`
-    query Posts {
-  posts {
+    query Posts($limit: Int!, $cursor: String) {
+  posts(cursor: $cursor, limit: $limit) {
     id
     createdAt
     updatedAt
     title
+    text
   }
 }
     `;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,15 @@ import React from "react";
 import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/crateUrqlClient";
 import NextLink from 'next/link'
-import { Link } from "@chakra-ui/react";
+import { Box, Heading, Link, Stack, Text } from "@chakra-ui/react";
 import { Layout } from "../components/Layout";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery();
+  const [{ data }] = usePostsQuery({
+    variables: {
+      limit: 10
+    }
+  });
   return (
     <Layout>
       <NextLink href="create-post">
@@ -16,7 +20,18 @@ const Index = () => {
       </Link>
       </NextLink>
       <div>Hello</div>
-      {!data ? null : data.posts.map((p) => <div key={p.id}>{p.title}</div>)}
+      {!data ? (
+        <div>Loading...</div>
+      ) : (
+        <Stack spacing={8}>
+          {data.posts.map((p) => (
+            <Box p={5} shadow="md" borderWidth="1px" key={p.id}>
+              <Heading fontSize="xl">{p.title}</Heading>
+              <Text mt={4}>{p.text}</Text>
+            </Box>
+          ))}
+        </Stack>
+      )}
     </Layout>
   );
 };
